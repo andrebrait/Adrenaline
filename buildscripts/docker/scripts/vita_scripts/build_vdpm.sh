409880a839eac2837f9026ea9a9f7d3edb3ae091
Andre Brait <andrebrait@gmail.com>
2020-08-14 16:57:18 +0200

    Isolate PSPDEV and VITASDK in Docker image

--- buildscripts/docker/scripts/vita_scripts/build_vdpm.sh
+++ buildscripts/docker/scripts/vita_scripts/build_vdpm.sh
@@ -5,13 +5,10 @@ set -euo pipefail
 IFS=$'\n\t'
 
 script_root="$(dirname "$(readlink -f $0)")"
-parent_dir="$(dirname "${script_root}")"
-. "${parent_dir}/utils.sh"
 WORKDIR="$(pwd)"
 cd "${script_root}"
 
 git clone --branch master https://github.com/vitasdk/vdpm.git
 cd vdpm
-store_git_rev "${WORKDIR}"
 ./bootstrap-vitasdk.sh
 ./install-all.sh
